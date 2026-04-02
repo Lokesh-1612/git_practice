a560f99827b99a6c47c35e0f3f7be85cd1648331
Lokesh-1612 <lokeshnayak1612@gmail.com>
2026-04-02 16:34:04 +0530

    myApp2 Added

---
+++ myApp2.java
@@ -0,0 +1,9 @@
+public class myApp2 {
+    int rollNumber = 10;
+    String name = "John";
+    public static void main(String[] args) {
+        myApp2 student = new myApp2();
+        System.out.println("Roll Number: " + student.rollNumber);
+        System.out.println("Name: " + student.name);
+    }
+}
